2301f2c22e2365431c648e71c6acaca466fe878e
Ari Porad <ari@ariporad.com>
2015-12-25 10:28:40 -0800

    fix: actually export stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var opts = require('babel-features').options();
 
 opts.plugins = opts.plugins.map(function (plugin) {
-  require('babel-plugin-' + plugin);
+  return require('babel-plugin-' + plugin);
 });
 
 module.exports = opts;
