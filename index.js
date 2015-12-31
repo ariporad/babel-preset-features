a3c947fb2563cf035baec6f1020dc4582f48cf3e
Ari Porad <ari@ariporad.com>
2015-12-31 08:42:37 -0800

    fix: account for hax/babel-features#7 until it's released

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var opts = require('babel-features').options();
 
 opts.plugins = opts.plugins.map(function (plugin) {
-  return require('babel-plugin-' + plugin);
+  return require('babel-plugin-' + plugin.toLowerCase()); // A safety check till hax/babel-features#7 gets merged.
 });
 
 module.exports = opts;
