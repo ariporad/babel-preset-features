ddc87281102be4a47b8982c4076c5245ecb53e08
Ryan Tsao <ryan.j.tsao@gmail.com>
2016-01-07 20:32:54 -0800

    Remove unnecessary toLowerCase on plugin names
    https://github.com/hax/babel-features/pull/7 has been merged

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var opts = require('babel-features').options();
 // TODO: Keep dependencies in package.json in sync with possible transforms from babel-features.
 
 opts.plugins = opts.plugins.map(function (plugin) {
-  return require('babel-plugin-' + plugin.toLowerCase()); // A safety check till hax/babel-features#7 gets merged.
+  return require('babel-plugin-' + plugin);
 });
 
 module.exports = opts;
