4e9ec0eb690353e1f81f37a141679b56b5cf6ea2
吴静波 <wujingbo@noahwm.com>
2016-06-07 16:56:24 +0800

    fix error while plugin have arguments

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var opts = require('babel-features').options();
 // TODO: Keep dependencies in package.json in sync with possible transforms from babel-features.
 
 opts.plugins = opts.plugins.map(function (plugin) {
-  return require('babel-plugin-' + plugin.toLowerCase()); // A safety check till hax/babel-features#7 gets merged.
+  if (typeof plugin === 'string') {
+    return require('babel-plugin-' + plugin.toLowerCase()); // A safety check till hax/babel-features#7 gets merged.
+  } else {  // Array here!!
+    return [require('babel-plugin-' + plugin[0].toLowerCase())].concat(plugin.slice(1));
+  }
 });
 
 module.exports = opts;
